1e9aaaa2f736b51e63e7f49c11c996c5580ce5a7
Paula <paula-lezcano@hotmail.com>
2021-04-19 19:27:07 -0300

    otro cambio

---
+++ html/historia.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="es" class="scroll-behavior">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../css/estilos.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+    <script src="https://unpkg.com/scrollreveal"></script>
+
+    <title>Historias</title>
+
+</head>
+
+<body class="fondoBody">
+
+    <nav class="navbar navcolor navbar-dark fixed-top">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="material-icons blanco">menu</span>
+        </button>
+        <a class="navbar-brand colorFuenteBlanca" href="../index.php">ABSOLUT</a>
+
+        <a class="material-icons blanco" href="../php/productos.php">shopping_cart</a>
+
+        <div class="collapse navbar-collapse fuenteHamburguesa" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link colorFuenteBlanca " href="../index.php">
+                        <span class="material-icons centro">home</span>
+                        Home</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link colorFuenteBlanca" href="productos.php">
+                        <span class="material-icons centro">shopping_cart</span>
+                        Productos</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link colorFuenteBlanca" href="recetas.php">
+                        <span class="material-icons centro">local_bar</span>
+                        Recetas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link colorFuenteBlanca" href="historia.php">
+                        <span class="material-icons centro">import_contacts</span>
+                        Historias</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link colorFuenteBlanca" href="contacto.php">
+                        <span class="material-icons centro">email</span>
+                        Contacto</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- foto principal -->
+    <header class="p-5">
+    
+        <div class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-sm-12 col-xl-6 text-center">
+
+                    <img src="../imagenes/origenes.jpg" class="img-fluid" alt="Botella de vidrio marca Absolut Vodka con gran cantidad de agua salpicada alrededor" width="500" height="500">
+
+
+                </div>
+
+                <div class="col-md-12 col-lg-12 col-xl-6 mt-5">
+
+                    <h1 class="display-4 text-center mt-4 mb-2">Historia</h1>
+                    <p class="lead mb-5">En 1879, Lars Olsson Smith introdujo la destilación continua con la que destilo Absolut Rent Brännvin (Absolute Pure Vodka). En vez de destilar el vodka tres o cuatro veces, lo habitual, se destilaba un número infinito de veces. Cien años más tarde, volvió a re-lanzarse como Absolut. Y al igual que entonces, Absolut sigue produciéndose en Åhus, la ciudad natal de L.O. Smith. Y también el lugar donde se cosecha el trigo con el que se produce el vodka. El modo de hacer Absolut no cambiará, como tampoco lo hará el auténtico sabor del vodka.
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row galeria p-0 ">
+                <div class="col-sm-6 col-lg-4 h-auto mb-4 ">
+
+                    <img src="../imagenes/historia1.jpg" alt="fakeimagen" width="300" height="300" class="mb-2 fixImages w-100 h-auto">
+
+                </div>
+                <div class="col-sm-6 col-lg-4 h-auto mb-4">
+
+                    <img src="../imagenes/historia2.jpg" alt="fakeimagen" width="300" height="300" class="mb-2 fixImages w-100 h-auto">
+
+                </div>
+                <div class="col-sm-6 col-lg-4 h-auto mb-4">
+
+                    <img src="../imagenes/historia3.jpg" alt="fakeimagen" width="300" height="300" class="mb-2 fixImages w-100 h-auto">
+
+                </div>
+                <div class="col-sm-6 col-lg-4 h-auto mb-4">
+
+                    <img src="../imagenes/historia4.jpg" alt="fakeimagen" width="300" height="300" class="mb-2 fixImages w-100 h-auto">
+
+                </div>
+                <div class="col-sm-6 col-lg-4 h-auto mb-4">
+
+                    <img src="../imagenes/historia6.jpg" alt="fakeimagen" width="300" height="300" class="mb-2 fixImages w-100 h-auto">
+
+                </div>
+                <div class="col-sm-6 col-lg-4 h-auto mb-4">
+
+                    <img src="../imagenes/historia5.jpg" alt="fakeimagen" width="300" height="300" class="mb-2 fixImages w-100 h-auto">
+
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+
+    <footer class="page-footer colorFooter mt-5">
+        <div class="bg-botonColor">
+            <div class="container">
+                <div class="row py-2 d-flex">
+                    <div class="col-md-12 text-center">
+                        <a href="https://es-la.facebook.com/AbsolutFanPage/"><span class="material-icons text-white mr-4">facebook</span></a>
+                        <a href="https://www.buenosaires.gob.ar/coronavirus"><span class="material-icons text-white mr-4">coronavirus</span></a>
+                        <p class="text-white">&copy; Copyright
+                            <a href="https://www.absolut.com/ar/" class="text-white">absolutvodka.com</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="js/jquery-3.5.1.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
